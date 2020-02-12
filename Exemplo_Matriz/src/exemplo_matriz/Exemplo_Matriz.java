6d3177f7b63b3af192326bdf9335d8db4cb8c32d
cfigueirasvalverde <cfigueirasvalverde@nautilus13.ad.danielcastelao.org>
2020-02-12 09:08:49 +0100

    Exemplo Matriz

---
+++ Exemplo_Matriz/src/exemplo_matriz/Exemplo_Matriz.java
@@ -0,0 +1,45 @@
+package exemplo_matriz;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author cfigueirasvalverde
+ */
+public class Exemplo_Matriz {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int[][]matriz = new int[3][4];
+      String[]alumnos={"alum1","alum2","alum3"};
+      String[]modulos={"M1","M2","M3","M4"};
+      int opcion;
+      
+      Metodos obx = new Metodos();
+         do{
+         opcion = Integer.parseInt(JOptionPane.showInputDialog("***MENU***\n1--> crear matriz" +"\n2--> amosar \n3--> amosar todo\n4-->Media Alumnos\n5-->Media Modulos \n6--> Sair \n Teclea a opcion"));
+         switch(opcion){
+ case 1:
+    matriz = obx.crearMatriz(matriz);
+    break;
+ case 2:
+    obx.amosar(matriz);
+    break;
+ case 3:
+     obx.amosarTodo(matriz, alumnos, modulos);
+     break;
+ case 4:
+     obx.amosarMediaAlum(obx.mediaAlumno(matriz));
+     break;
+ case 5:
+     obx.amosarMediaModulos(obx.mediaModulos(matriz));
+     break;
+ case 6:
+    obx.sair();
+    }
+    }while(opcion<6);
+        
+}    
+}
---
+++ Exemplo_Matriz/src/exemplo_matriz/Metodos.java
@@ -0,0 +1,99 @@
+package exemplo_matriz;
+
+/**
+ *
+ * @author cfigueirasvalverde
+ */
+class Metodos {
+    public int[][] crearMatriz(int[][] taboa){
+    for (int f=0;f<taboa.length;f++){
+      for(int c=0;c<taboa[f].length;c++){
+      taboa[f][c]=Pedir.pedirInt();
+      }    
+    }    
+return taboa;
+}
+
+public void amosar(int[][]taboa){
+for (int f=0;f<taboa.length;f++){
+    System.out.println("\n");
+      for(int c=0;c<taboa[f].length;c++){
+          System.out.print(taboa[f][c]+"  ");
+      }    
+    }
+}
+
+public void sair(){
+System.exit(0);
+}
+
+public String[] arrayFila(int [][]taboa){
+    String []filas = new String [taboa.length];
+    for(int i=0;i<filas.length;i++){
+    filas[i]=Pedir.pedirString("nome filas ");
+    }
+return filas;
+}
+
+public String[] arrayColumna(int [][]taboa){
+    String []columnas = new String [taboa[0].length];
+    for(int i=0;i<columnas.length;i++){
+    columnas[i]=Pedir.pedirString("nome columnas ");
+    }
+return columnas;
+}
+
+public void amosarTodo(int[][]taboa,String[]alumnos,String[]modulos){
+    System.out.print("    ");
+    for(int i=0;i<modulos.length;i++){
+    System.out.print("   "+modulos[i]);    
+}
+  for (int f=0;f<taboa.length;f++){
+    System.out.print("\n "+alumnos[f]);
+      for(int c=0;c<taboa[f].length;c++){
+          System.out.print("  "+taboa[f][c]+"  ");
+      }    
+    }
+    
+}
+
+public int[] mediaAlumno(int[][]taboa){
+int[]mediaAlum= new int[taboa.length];       
+ for (int f=0;f<taboa.length;f++){
+      for(int c=0;c<taboa[f].length;c++){
+         mediaAlum[f]=mediaAlum[f]+taboa[f][c];  
+         
+      }
+      mediaAlum[f]=mediaAlum[f]/4;
+    }   
+return mediaAlum;
+}
+public void amosarMediaAlum(int[]taboa){
+    System.out.println("\nMedia alumnos: ");
+    for (int i=0;i<taboa.length;i++){
+    System.out.print(taboa[i]+"  ");
+     
+      }    
+    }
+
+public int[] mediaModulos(int[][]taboa){
+int[]mediaModulos= new int[4];
+int acu=0;
+ for (int c=0;c<mediaModulos.length;c++){ 
+ acu=0;
+ for(int f=0;f<taboa.length;f++){
+ acu=acu+taboa[f][c];
+ }
+ mediaModulos[c]=acu/3;
+ }
+return mediaModulos;
+}
+
+public void amosarMediaModulos(int[]taboa){
+    System.out.println("\nMedia modulos: ");
+    for (int i=0;i<taboa.length;i++){
+    System.out.print(taboa[i]+"  ");
+     
+      }    
+    }
+}
